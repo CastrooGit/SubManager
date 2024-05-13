ec0a226d63e97c7b53017d46aaf0bbb8d40d6b66
AfonsoCastro2006 <jokeresqueleto@gmail.com>
2024-05-13 15:16:13 +0100

    next

--- bin/run_all.py
+++ bin/run_all.py
@@ -2,26 +2,20 @@
 
 import subprocess
 import os
-import argparse
-
-def run_all():
-    # Get the directory where this script is located
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    project_dir = os.path.dirname(script_dir)
-
-    # Run Subscription_API.py
-    subprocess.Popen(["python3", os.path.join(project_dir, "Subscription_API.py")])
-
-    # Run SubscriptionChecker.py
-    subprocess.Popen(["python3", os.path.join(project_dir, "SubscriptionChecker.py")])
 
 def main():
-    parser = argparse.ArgumentParser(description='Run Subscription Manager')
-    parser.add_argument('--run', action='store_true', help='Run Subscription Manager')
-    args = parser.parse_args()
+    # Get the directory where this script is located
+    script_dir = os.path.dirname(os.path.realpath(__file__))
 
-    if args.run:
-        run_all()
+    # Run Subscription_API.py
+    api_process = subprocess.Popen([os.path.join(script_dir, "Subscription_API.py")])
+
+    # Run SubscriptionChecker.py
+    checker_process = subprocess.Popen([os.path.join(script_dir, "SubscriptionChecker.py")])
+
+    # Wait for both processes to finish
+    api_process.wait()
+    checker_process.wait()
 
 if __name__ == "__main__":
     main()
